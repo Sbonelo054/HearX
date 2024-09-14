ee6b76b996a4be41da4b7636543dfa489e88fb94
Nhlanhla Nqondo <sbonelongqondo54@gmail.com>
2024-09-14 21:03:59 +0200

    Koin Dependency Injection

---
+++ app/src/main/java/com/hearx/din/util/ApplicationHearXApplication.kt
@@ -0,0 +1,18 @@
+package com.hearx.din.util
+
+import android.app.Application
+import com.hearx.din.dependencyinjection.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class ApplicationHearXApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@ApplicationHearXApplication)
+            modules(viewModelModule)
+        }
+    }
+}
