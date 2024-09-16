72539f9c7ca9c5ad2a19b9f9530ecd9205dcb0c7
Nhlanhla Nqondo <sbonelongqondo54@gmail.com>
2024-09-16 13:35:32 +0200

    Refactored Code

--- app/src/main/java/com/hearx/din/repository/ScoreRepository.kt
+++ app/src/main/java/com/hearx/din/repository/ScoreRepository.kt
@@ -3,5 +3,5 @@ package com.hearx.din.repository
 import com.hearx.din.model.ResultsData
 
 interface ScoreRepository {
-    suspend fun submitScore(resultsData: ResultsData)
+    suspend fun submitScore(resultsData: ResultsData): String
 }
