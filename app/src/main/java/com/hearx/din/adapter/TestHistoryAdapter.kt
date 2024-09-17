c5d29fa786cced2d276054665a0e3618540a1626
Nhlanhla Nqondo <sbonelongqondo54@gmail.com>
2024-09-17 03:20:59 +0200

    Score Submitted

--- app/src/main/java/com/hearx/din/adapter/TestHistoryAdapter.kt
+++ app/src/main/java/com/hearx/din/adapter/TestHistoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.hearx.din.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hearx.din.R
 import com.hearx.din.database.TestHistoryTable
 
-class TestHistoryAdapter(private val historyList: List<TestHistoryTable>): RecyclerView.Adapter<TestHistoryAdapter.TestHistoryViewHolder>() {
+class TestHistoryAdapter(private val context: Context, private val historyList: List<TestHistoryTable>): RecyclerView.Adapter<TestHistoryAdapter.TestHistoryViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestHistoryAdapter.TestHistoryViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.test_history_item, parent, false)
@@ -17,13 +18,9 @@ class TestHistoryAdapter(private val historyList: List<TestHistoryTable>): Recyc
 
     override fun onBindViewHolder(holder: TestHistoryAdapter.TestHistoryViewHolder, position: Int) {
         val history = historyList[position]
-        val date = "Test was taken on: ${history.testDate}"
-        val coordinates = "Your score was: ${history.score}"
-        val tripletPlayed = "Last triplet played was: ${history.played}"
-        val tripletAnswered = "Last triplet answered was: ${history.answered}"
+        val date = context.getString(R.string.test_was_taken_on, history.testDate)
+        val coordinates = context.getString(R.string.your_score_was, history.score)
         holder.date.text = date
-        holder.played.text = tripletPlayed
-        holder.answered.text = tripletAnswered
         holder.score.text = coordinates
     }
 
@@ -34,7 +31,5 @@ class TestHistoryAdapter(private val historyList: List<TestHistoryTable>): Recyc
     inner class TestHistoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var date: TextView = itemView.findViewById(R.id.history_date)
         var score: TextView = itemView.findViewById(R.id.history_score)
-        var played: TextView = itemView.findViewById(R.id.history_played)
-        var answered: TextView = itemView.findViewById(R.id.history_answered)
     }
 }
